0ca2a68b13e7aacf72c95bc54cea8056820daabb
CraftyDragon678 <craftydragon678@gmail.com>
2020-11-10 00:39:32 +0900

    feat: add login route

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Route, Switch } from "react-router";
 import { BrowserRouter } from "react-router-dom";
-import { Root } from "../pages";
+import { Root, Login } from "../pages";
 
 const App: React.FC = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Root} />
+      <Route exact path="/login" component={Login} />
     </Switch>
   </BrowserRouter>
 );
