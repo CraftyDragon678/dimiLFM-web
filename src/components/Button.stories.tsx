9a6ae75475b1890f72152e12c349e1e9485c0d55
CraftyDragon678 <craftydragon678@gmail.com>
2020-11-09 11:21:14 +0900

    chore: chnage storybook extension

--- src/components/Button.stories.tsx
+++ src/components/Button.stories.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { text } from "@storybook/addon-knobs";
 import { action } from "@storybook/addon-actions";
-import { Button } from "./Button";
+import Button from "./Button";
 
 export default {
   title: "Button",
