0ff79e1a5d208a18623ea7e49d4e6a57d04560f8
CraftyDragon678 <craftydragon678@gmail.com>
2020-11-09 12:12:39 +0900

    refactor: use router

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Router from './router';
+
+import "./styles/reset.css";
+import "./styles/index.css";
+
+export default () => (
+  <Router />
+);
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
-import App from './client/App';
+import App from './App';
 
 import './styles/reset.css';
 import './styles/index.css';
