1f622b0db62fb124268e9d53b28d7c725e8e68a4
CraftyDragon678 <craftydragon678@gmail.com>
2020-11-15 00:41:02 +0900

    feat: add header in root page

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,3 +1,28 @@
-const Root = () => null;
+import React from 'react';
+import styled from '@emotion/styled';
+import variables from '../styles/variables';
+import SearchBox from '../components/SearchBox';
 
-export default Root;
+const Header = styled.div`
+  height: 100px;
+  border-bottom: 1px solid ${variables.borderColor};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background-color: white;
+`;
+
+const Logo = styled.div`
+  font-size: 48px;
+  color: ${variables.logoColor};
+  font-weight: bold;
+  margin-right: 20px;
+`;
+
+export default () => (
+  <Header>
+    <Logo>L&FM</Logo>
+    <SearchBox />
+  </Header>
+);
