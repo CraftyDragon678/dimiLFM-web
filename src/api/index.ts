94edfad47f1f7ad1143d8cbd1d3c80b0f0a24fef
CraftyDragon678 <craftydragon678@gmail.com>
2020-11-17 19:09:25 +0900

    chore: change api address

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://cragon.kro.kr:8080",
+  withCredentials: true,
 });
 
 api.interceptors.request.use((config) => {
