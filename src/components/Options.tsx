5e49ddeb2a1df3872d0f41e073d07e671efcca72
CraftyDragon678 <craftydragon678@gmail.com>
2020-11-17 22:55:46 +0900

    feat: mock options

---
+++ src/components/Options.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ToggleButton from './ToggleButton';
+
+
+const Options = () => {
+  return (
+    <div>
+      <ToggleButton />
+      <span>완료 항목</span>
+    </div>
+  );
+};
+
+export default Options;
