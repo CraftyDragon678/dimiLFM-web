beeb1b239b5c9be039d622072c3c416e366b2fa9
CraftyDragon678 <craftydragon678@gmail.com>
2020-11-17 19:36:31 +0900

    feat: use history

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { Route, Switch } from "react-router";
-import { BrowserRouter } from "react-router-dom";
+import { Router } from "react-router-dom";
 import { Root, Login, Chat, NotFound } from "../pages";
+import history from "./history";
 
 const App: React.FC = () => (
-  <BrowserRouter>
+  <Router history={history}>
     <Switch>
       <Route exact path="/" component={Root} />
       <Route exact path="/chat" component={Chat} />
       <Route exact path="/login" component={Login} />
       <NotFound />
     </Switch>
-  </BrowserRouter>
+  </Router>
 );
 
 export default App;
