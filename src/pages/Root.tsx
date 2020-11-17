e2a231c3f9989ff870d59ad0089b0215f718c1a0
CraftyDragon678 <craftydragon678@gmail.com>
2020-11-17 22:57:15 +0900

    feat: make fab

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from '@emotion/styled';
 import variables from '../styles/variables';
 import SearchBox from '../components/SearchBox';
+import Gallery from '../components/Gallery';
+import ToggleButton from '../components/ToggleButton';
+import Options from '../components/Options';
+import Fab from '../components/Fab';
+import newSvg from '../assets/images/new.svg';
+import chatSvg from '../assets/images/chat.svg';
 
 const Header = styled.div`
   height: 100px;
@@ -20,9 +26,58 @@ const Logo = styled.div`
   margin-right: 20px;
 `;
 
-export default () => (
-  <Header>
-    <Logo>L&FM</Logo>
-    <SearchBox />
-  </Header>
-);
+const FloatWrapper = styled.div`
+  position: fixed;
+  right: 24px;
+  bottom: 24px;
+  display: grid;
+  grid-row-gap: 24px;
+`;
+
+const Icon = styled.img`
+  width: 48px;
+  height: 48px;
+`;
+
+const WriteBoardWrapper = styled.div`
+  width: 200px;
+  padding: 16px;
+  background-color: white;
+
+  border: 1px solid ${variables.lightGray};
+  border-radius: 8px;
+  box-shadow: 0 3px 5px rgba(0, 0, 0, .2);
+  display: grid;
+`;
+
+export default () => {
+  const [showWriteBoard, setShowWriteBoard] = useState(false);
+
+  const WriteBoard = () => (
+    <WriteBoardWrapper>
+      <div>분실물 찾아가세요</div>
+      <div>분실물 찾아주세요</div>
+      <div>판매합니다</div>
+    </WriteBoardWrapper>
+  );
+
+  return (
+    <div>
+      <Header>
+        <Logo>L&FM</Logo>
+        <SearchBox />
+      </Header>
+      <Options />
+      <FloatWrapper>
+        {showWriteBoard && <WriteBoard />}
+        <Fab onClick={() => setShowWriteBoard(!showWriteBoard)}>
+          <Icon src={newSvg} />
+        </Fab>
+        <Fab>
+          <Icon src={chatSvg} />
+        </Fab>
+      </FloatWrapper>
+      <Gallery />
+    </div>
+  );
+};
