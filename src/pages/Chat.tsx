ad8e2ad67be7a9a7db87ba66f2caa2e85e1d2f89
CraftyDragon678 <craftydragon678@gmail.com>
2020-11-19 07:28:23 +0900

    feat: chat socket connect

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import api from '../api';
 
 export default () => {
-  const sendMessage = () => {
+  const ws = useRef<WebSocket>();
 
+  useEffect(() => {
+    (async () => {
+      const { status, data } = await api.get('/auth/chat');
+      if (status === 401) {
+        return;
+      }
+
+      ws.current = new WebSocket('wss://cragon.kro.kr:8080/socket');
+    })();
+  }, []);
+
+  const sendMessage = () => {
+    ws.current?.send("I'm like tt");
   }
 
   return (
     <div>
       <input onKeyUp={(e) => e.key === 'Enter'}></input>
+      <button onClick={sendMessage} />
     </div>
   );
 };
