f56dacbca2734fdad9001d687366e52231bdfff6
CraftyDragon678 <craftydragon678@gmail.com>
2020-11-22 14:33:37 +0900

    feat: add text component

---
+++ src/components/Text.tsx
@@ -0,0 +1,8 @@
+import styled from "@emotion/styled";
+import variables from "../styles/variables";
+
+export const SubTitle = styled.div`
+  font-size: 30px;
+  font-weight: bold;
+  color: ${variables.logoColor};
+`;
