495509d60ec2465b5bd32606a888ccb9df49024d
CraftyDragon678 <craftydragon678@gmail.com>
2020-11-22 18:53:20 +0900

    feat: make home, goBack button in notfound screen

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import styled from "@emotion/styled";
+import { Link } from "react-router-dom";
+import Button from "../components/Button";
+import history from "../router/history";
 
 const Container = styled.div`
   min-height: 100vh;
@@ -26,9 +29,31 @@ const SmallText = styled.div`
   color: white;
 `;
 
+const Home = styled(Link)`
+  color: white;
+  text-decoration: none;
+`;
+
+const Row = styled.div`
+  margin-top: 50px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-column-gap: 20px;
+
+  ${Button} {
+    font-size: 50px;
+    width: 200px;
+    height: 80px;
+  }
+`;
+
 export default () => (
   <Container>
     <BigText>404</BigText>
     <SmallText>이런! 보성이가 아직 개발 안 했어요</SmallText>
+    <Row>
+      <Home to="/"><Button>홈으로</Button></Home>
+      <Button onClick={() => history.goBack()}>이전</Button>
+    </Row>
   </Container>
 );
