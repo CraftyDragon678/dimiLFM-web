43d068f31be3a560087ce3cf464bda3f49b0ab7e
CraftyDragon678 <craftydragon678@gmail.com>
2020-11-24 16:39:19 +0900

    feat: add set floor button

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,16 +1,22 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import styled from '@emotion/styled';
 import ReactTooltip from 'react-tooltip';
 import { MapData } from '../data/map';
 import bonData from '../data/map/bon';
+import variables from '../styles/variables';
+import Button from './Button';
 
 interface MapProps {
   enable: boolean;
 }
 
 const Svg = styled.svg<MapProps>`
-  transition: 300ms transform ease;
-  transform: ${({ enable }) => enable && 'perspective(500px) translate3D(200px, 0, 0) rotate3d(3, -1, 1, 30deg)'};
+  transition: .5s transform ease, .5s opacity ease, 1s visibility ease;
+  height: 400px;
+  transform: ${({ enable }) => enable && 'perspective(500px) translate3D(250px, -100px, 0) rotate3d(3, -1, 1, 30deg)'};
+  opacity: ${({ enable }) => enable || 0};
+  visibility: ${({ enable }) => enable || 'hidden'};
+  position: absolute;
 
   rect,
   polygon,
@@ -19,6 +25,7 @@ const Svg = styled.svg<MapProps>`
     fill: white;
     stroke: black;
     position: relative;
+    pointer-events: ${({ enable }) => enable || 'none'};
 
     :not([data-ignore]):hover {
       fill: red;
@@ -26,35 +33,59 @@ const Svg = styled.svg<MapProps>`
   }
 `;
 
-const genMap = (map: MapData) => ({ enable }: MapProps) => (
-  <Svg version="1.1" width={map.data.width} height={map.data.height} enable={enable}>
-    {Object.entries(map.map).map(([name, Element]) => (
-      {
-        ...Element,
-        props: {
-          ...Element.props,
-          id: `${map.data.prefix}-${name}`,
-        },
-        key: name,
-      }
-    ))}
-  </Svg>
-);
+const genMap = (map: MapData) => ({ enable }: MapProps) => {
+  console.log('teststsetsetse')
+  return (
+    <Svg version="1.1" viewBox={`0 0 ${map.data.width} ${map.data.height}`} enable={enable}>
+      {Object.entries(map.map).map(([name, Element]) => (
+        {
+          ...Element,
+          props: {
+            ...Element.props,
+            id: `${map.data.prefix}-${name}`,
+          },
+          key: name,
+        }
+      ))}
+    </Svg>
+  )
+};
 
 const genMaps = (maps: MapData[]) => maps.map((map) => genMap(map));
 
-const Map = (maps: MapData[]) => {
-  const [floor, setFloor] = useState(0);
-  const mapsFC: React.FC<MapProps>[] = genMaps(maps);
+const MapItem = styled.div`
+  display: relative;
+`;
+
+const Floor = styled(Button)`
+
+`;
+
+const Container = styled.div`
+  width: 800px;
+  height: 500px;
+  margin: 16px;
+  border: 1px solid ${variables.borderColor};
+  border-radius: 16px;
+`;
+
+const Map: React.FC<{maps: MapData[]}> = ({ maps }) => {
+  const [floor, setFloor] = useState(-1);
+  const mapsFC: React.FC<MapProps>[] = useMemo(() => genMaps(maps), [maps]);
 
   return (
-    <div>
+    <Container>
       {mapsFC.map((MapFC, idx) => (
-        <MapFC key={maps[idx].data.prefix} enable={floor === idx} />
+        <MapItem key={maps[idx].data.prefix}>
+          <Floor onClick={() => setFloor(idx)}>{`${idx + 1}층`}</Floor>
+        </MapItem>
+      ))}
+      {mapsFC.map((MapFC, idx) => (
+        <MapFC key={maps[idx].data.prefix} enable={idx === floor} />
       ))}
       <ReactTooltip effect="solid" />
-    </div>
+    </Container>
   );
 }
 
-export default () => Map(bonData);
+export default () => <Map maps={bonData} />;
