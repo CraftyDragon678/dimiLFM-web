f6a37d7b87d3316046092e7e34768e65f93ed9df
CraftyDragon678 <craftydragon678@gmail.com>
2020-11-27 01:58:56 +0900

    feat: getName also returns floor

--- src/utils/map/index.ts
+++ src/utils/map/index.ts
@@ -1,5 +1,10 @@
 import mapData from '../../data/map';
 
-export const getName = (id: string): string => (
-  Object.entries(mapData.flat().find((e) => e.data.prefix === id.split('/')[0])?.map || {}).find(([k]) => k === id.split('/')[1])?.[1].props['data-tip']
-);
+export const getName = (id: string): string => {
+  const floor = mapData.flat().find((e) => e.data.prefix === id.split('/')[0]);
+
+  const room = Object.entries(floor?.map || {}).find(([k]) => k === id.split('/')[1])?.[1];
+
+  if (!room) return '';
+  return `${floor?.data.floor}층 ${room.props['data-tip']}`;
+};
