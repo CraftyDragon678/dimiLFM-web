18e3564067afaf8e07cc9f7eaa0d0cd40827e7f5
CraftyDragon678 <craftydragon678@gmail.com>
2020-11-26 13:34:34 +0900

    feat: add magenta color (for selected in map)

--- src/styles/variables.ts
+++ src/styles/variables.ts
@@ -6,7 +6,8 @@ export default {
   borderColor: '#707070',
   background: '#f0f2f5',
   error: '#ff3939',
-  logoColor: '#5258CB',
+  logoColor: '#5258cb',
   pink: '#ffceee',
+  magenta: '#d9367f',
   gradient: (direction: string) => `linear-gradient(${direction}, #4e54c8 0%, #8f94fb 100%)`,
 };
