5e7fb6059a29fb4403e6f9bad0697773a2a361ec
CraftyDragon678 <craftydragon678@gmail.com>
2020-11-28 09:30:32 +0900

    feat: write using stage

--- src/pages/write/Found/index.tsx
+++ src/pages/write/Found/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styled from '@emotion/styled';
+import WriteIndicator from 'src/components/WriteIndicator';
 import Box from '../../../components/Box';
 import variables from '../../../styles/variables';
 import First, { FirstProps } from './First';
@@ -15,6 +16,18 @@ const Upper = styled(Box)`
   height: 200px;
   background-color: ${variables.logoColor};
   color: white;
+  display: flex;
+  align-items: center;
+  padding: 60px;
+  box-sizing: border-box;
+`;
+
+const Title = styled.div`
+  font-size: 45px;
+  font-weight: bold;
+  color: white;
+  line-height: 1.5;
+  flex: 1;
 `;
 
 const Content = styled(Box)`
@@ -41,22 +54,50 @@ export default () => {
       foundLocation: undefined,
     },
   });
-  const [valid, setValid] = useState(false);
+  const [valid, setValid] = useState([false, false]);
+  const [stage, setStage] = useState(0);
+
+  const setIndexedValid = (index: number) => (value: boolean) => {
+    setValid([...valid.slice(0, index), value, ...valid.slice(index + 1)]);
+  };
+
+  const stages = [
+    <First
+      verify={setIndexedValid(0)}
+      data={data.first}
+      dataHandler={(newdata) => setData({ ...data, first: newdata })}
+    />,
+    <First
+      verify={setIndexedValid(1)}
+      data={data.first}
+      dataHandler={(newdata) => setData({ ...data, first: newdata })}
+    />,
+  ];
 
   return (
     <Wrapper>
-      <Upper>분실물 찾아가세요</Upper>
+      <Upper>
+        <Title>
+          분실물
+          <br />
+          찾아가세요
+        </Title>
+        <WriteIndicator index={stage} stage={['발견 설정', '내용 작성', '희망 장소', '완료']} />
+      </Upper>
       <Content shadow>
         <ContentArrow>
-          <Arrow left disable />
-          <Arrow disable={!valid} />
+          <Arrow
+            left
+            disable={stage === 0}
+            onClick={() => setStage(stage - 1)}
+          />
+          <Arrow
+            disable={!valid[stage]}
+            onClick={() => setStage(stage + 1)}
+          />
         </ContentArrow>
         <ContentWrapper>
-          <First
-            verify={(isValid) => setValid(isValid)}
-            data={data.first}
-            dataHandler={(newdata) => setData({ ...data, first: newdata })}
-          />
+          {stages[stage]}
         </ContentWrapper>
       </Content>
     </Wrapper>
