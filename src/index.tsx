8dde03e7b0b30f8eb4278a2ed44d1f9048f89fec
CraftyDragon678 <craftydragon678@gmail.com>
2020-11-28 09:52:13 +0900

    feat: add tui css

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'codemirror/lib/codemirror.css';
+import '@toast-ui/editor/dist/toastui-editor.css';
+
 import * as serviceWorker from './serviceWorker';
 import App from './App';
 
