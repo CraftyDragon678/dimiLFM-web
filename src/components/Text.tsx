2d7b7fe9ecc1eef0c4f6be15bc8245491b4648fd
CraftyDragon678 <craftydragon678@gmail.com>
2020-11-28 12:39:39 +0900

    feat: add description text component

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -5,4 +5,11 @@ export const SubTitle = styled.div`
   font-size: 30px;
   font-weight: bold;
   color: ${variables.logoColor};
+  margin-bottom: 10px;
+`;
+
+export const Description = styled.div`
+  font-size: 18px;
+  color: ${variables.logoColor};
+  line-height: 1.5;
 `;
