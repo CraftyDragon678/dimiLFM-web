85d1d18b7bc85152630afde539fb5a79ea7c2985
CraftyDragon678 <craftydragon678@gmail.com>
2020-11-28 18:34:21 +0900

    feat: use wrapper

--- src/pages/write/Found/index.tsx
+++ src/pages/write/Found/index.tsx
@@ -1,54 +1,13 @@
-import React, { useState } from 'react';
-import styled from '@emotion/styled';
-import WriteIndicator from 'src/components/WriteIndicator';
+import React from 'react';
+import { Viewer } from '@toast-ui/react-editor';
 import { Description, SubTitle } from 'src/components/Text';
+import { TitleInput } from 'src/components/Input';
 import { getRangeText } from 'src/utils/date';
 import { getName } from 'src/data/map';
-import { Viewer } from '@toast-ui/react-editor';
-import { TitleInput } from 'src/components/Input';
-import Box from '../../../components/Box';
-import variables from '../../../styles/variables';
 import First, { FirstProps } from './First';
 import Second, { SecondProps } from './Second';
 import Third, { ThirdProps } from './Third';
-import Arrow from '../../../components/Arrow';
-
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  flex: 1;
-`;
-
-const Upper = styled(Box)`
-  height: 200px;
-  background-color: ${variables.logoColor};
-  color: white;
-  display: flex;
-  align-items: center;
-  padding: 60px;
-  box-sizing: border-box;
-`;
-
-const Title = styled.div`
-  font-size: 45px;
-  font-weight: bold;
-  color: white;
-  line-height: 1.5;
-  flex: 1;
-`;
-
-const Content = styled(Box)`
-  margin-top: 16px;
-`;
-
-const ContentArrow = styled.div`
-  display: flex;
-  justify-content: space-between;
-`;
-
-const ContentWrapper = styled.div`
-  margin: 64px;
-`;
+import WriteWrapper from '../WriteWrapper';
 
 interface WriteFoundData {
   first: FirstProps;
@@ -56,95 +15,51 @@ interface WriteFoundData {
   third: ThirdProps;
 }
 
-export default () => {
-  const [data, setData] = useState<WriteFoundData>({
-    first: {
-      foundDate: [new Date(), new Date()],
-      foundLocation: undefined,
-    },
-    second: {
-      title: '',
-      content: '',
-    },
-    third: {
-      radioIndex: -1,
-      foundLocation: undefined,
-    },
-  });
-  const [valid, setValid] = useState([false, false, false, true]);
-  const [stage, setStage] = useState(0);
-
-  const setIndexedValid = (index: number) => (value: boolean) => {
-    setValid([...valid.slice(0, index), value, ...valid.slice(index + 1)]);
-  };
-
-  const updateData = <T extends keyof WriteFoundData>(key: T) => (
-    (newdata: WriteFoundData[T] | ((prev: WriteFoundData[T]) => WriteFoundData[T])) => (
-      setData((prev) => ({ ...prev, [key]: typeof newdata === 'function' ? newdata(prev[key]) : newdata }))
-    ));
-
-  const stages = [
-    <First
-      verify={setIndexedValid(0)}
-      data={data.first}
-      dataHandler={updateData('first')}
-    />,
-    <Second
-      verify={setIndexedValid(1)}
-      data={data.second}
-      dataHandler={updateData('second')}
-    />,
-    <Third
-      verify={setIndexedValid(2)}
-      data={data.third}
-      dataHandler={updateData('third')}
-    />,
-    <>
-      <SubTitle>발견 설정</SubTitle>
-      <Description>
-        {getRangeText(data.first.foundDate)}
-        <br />
-        {data.first.foundLocation && getName(data.first.foundLocation)}
-      </Description>
-      <SubTitle>희망 장소</SubTitle>
-      <Description>
-        {data.third.foundLocation
-          ? getName(data.third.foundLocation)
-          : '발견 장소에 그대로 두었습니다.'}
-      </Description>
-      <SubTitle>작성글 미리보기</SubTitle>
-      <TitleInput defaultValue={data.second.title} disabled />
-      <Viewer initialValue={data.second.content} />
-    </>,
-  ];
-
-  return (
-    <Wrapper>
-      <Upper>
-        <Title>
-          분실물
+export default () => (
+  <WriteWrapper
+    stages={{
+      first: First, second: Second, third: Third,
+    }}
+    final={({ data }) => (
+      <>
+        <SubTitle>발견 설정</SubTitle>
+        <Description>
+          {getRangeText(data.first.foundDate)}
           <br />
-          찾아가세요
-        </Title>
-        <WriteIndicator index={stage} stage={['발견 설정', '내용 작성', '희망 장소', '완료']} />
-      </Upper>
-      <Content shadow>
-        <ContentArrow>
-          <Arrow
-            left
-            disable={stage === 0}
-            onClick={() => setStage(stage - 1)}
-          />
-          <Arrow
-            check={stage === 3}
-            disable={!valid[stage]}
-            onClick={() => setStage(stage + 1)}
-          />
-        </ContentArrow>
-        <ContentWrapper>
-          {stages[stage]}
-        </ContentWrapper>
-      </Content>
-    </Wrapper>
-  );
-};
+          {data.first.foundLocation && getName(data.first.foundLocation)}
+        </Description>
+        <SubTitle>희망 장소</SubTitle>
+        <Description>
+          {data.third.foundLocation
+            ? getName(data.third.foundLocation)
+            : '발견 장소에 그대로 두었습니다.'}
+        </Description>
+        <SubTitle>작성글 미리보기</SubTitle>
+        <TitleInput defaultValue={data.second.title} disabled />
+        <Viewer initialValue={data.second.content} />
+      </>
+    )}
+    title={(
+      <>
+        분실물
+        <br />
+        찾아가세요
+      </>
+    )}
+    initialData={{
+      first: {
+        foundDate: [new Date(), new Date()],
+        foundLocation: undefined,
+      },
+      second: {
+        title: '',
+        content: '',
+      },
+      third: {
+        radioIndex: -1,
+        foundLocation: undefined,
+      },
+    } as WriteFoundData}
+    stageLabels={['발견 설정', '내용 작성', '희망 장소', '완료']}
+  />
+);
