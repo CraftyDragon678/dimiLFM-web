051b89d39f3615523ffe846b83e9d63aec7a21ea
CraftyDragon678 <craftydragon678@gmail.com>
2020-11-28 09:29:56 +0900

    feat: arrow trigger onClick when only enable

--- src/components/Arrow.tsx
+++ src/components/Arrow.tsx
@@ -16,7 +16,7 @@ const Svg = styled.svg<{enable: boolean}>`
 `;
 
 const Arrow: React.FC<ArrowProps> = ({ left, disable, onClick }) => (
-  <Svg version="1.1" width="27.3px" height="15.5px" enable={!disable} onClick={onClick}>
+  <Svg version="1.1" width="27.3px" height="15.5px" enable={!disable} onClick={(e) => disable || (onClick && onClick(e))}>
     {left ? (
       <>
         <polyline points="26.3,7.8 1,7.8 7.8,1" />
