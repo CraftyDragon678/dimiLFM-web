05ca29607d14cea47a19dd1b38974cb5b5256684
CraftyDragon678 <craftydragon678@gmail.com>
2020-11-29 18:16:34 +0900

    feat: support tag

--- src/pages/write/Second.tsx
+++ src/pages/write/Second.tsx
@@ -1,28 +1,47 @@
+import styled from '@emotion/styled';
 import { Editor } from '@toast-ui/react-editor';
 import React, { useRef } from 'react';
 import { TitleInput } from 'src/components/Input';
+import Select from 'src/components/Select';
 import { SubTitle } from 'src/components/Text';
+import { Tag } from 'src/data/tags';
 import { WriteProps } from '../../types/write';
 
 export interface SecondProps {
   title: string;
   content: string;
+  tag: Tag;
 }
 
-const Second: React.FC<WriteProps<SecondProps>> = ({ verify, data, dataHandler }) => {
+const TitleWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 3fr 1fr;
+  grid-column-gap: 10px;
+`;
+
+const Second: React.FC<WriteProps<SecondProps>> = ({
+  verify, data, dataHandler, tags,
+}) => {
   const editorEl = useRef<Editor>(null);
 
   return (
     <>
       <SubTitle>내용 작성</SubTitle>
-      <TitleInput
-        value={data.title}
-        onChange={(e) => {
-          dataHandler({ ...data, title: e.target.value });
-          verify(!!e.target.value && !!data.content);
-        }}
-        placeholder="제목"
-      />
+      <TitleWrapper>
+        <TitleInput
+          value={data.title}
+          onChange={(e) => {
+            dataHandler({ ...data, title: e.target.value });
+            verify(!!e.target.value && !!data.content);
+          }}
+          placeholder="제목"
+        />
+        <Select
+          index={tags.indexOf(data.tag as any)}
+          options={tags as any}
+          onChange={(index) => dataHandler({ ...data, tag: tags[index] })}
+        />
+      </TitleWrapper>
       <Editor
         placeholder="내용을 입력하세요"
         events={{
