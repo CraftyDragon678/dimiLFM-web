ebd0d47fe99189240ce55520a44a3a0b407a1347
CraftyDragon678 <craftydragon678@gmail.com>
2020-11-30 22:24:06 +0900

    feat: chat page layout

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,52 +1,88 @@
+import styled from '@emotion/styled';
 import React, { useEffect, useRef, useState } from 'react';
+import variables from 'src/styles/variables';
 import api from '../api';
 
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  background-color: white;
+`;
+
+const ListContainer = styled.div`
+  width: 300px;
+`;
+
+const UserWrapper = styled.div<{enable: boolean}>`
+  display: flex;
+  align-items: center;
+  padding: 10px 50px;
+  border-radius: 10px 0 0 10px;
+  background-color: ${({ enable }) => enable && variables.lightPurple};
+`;
+
+const UserImage = styled.img`
+  width: 50px;
+  height: 50px;
+  border: 1px solid ${variables.borderColor};
+  border-radius: 25px;
+  margin-right: 10px;
+`;
+
+const UserName = styled.div`
+  > span {
+    :first-of-type {
+      font-size: 18px;
+    }
+    :last-of-type {
+      font-size: 10px;
+    }
+  }
+  > div {
+    color: #999;
+    font-size: 14px;
+    margin-top: 4px;
+  }
+`;
+
+const MessageContainer = styled.div`
+`;
+
 export default () => {
-  const ws = useRef<WebSocket>();
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState<string[]>([]);
 
-  useEffect(() => {
-    (async () => {
-      const { status, data } = await api.get('/auth/socket');
-      if (status === 401) {
-        return;
-      }
-
-      ws.current = new WebSocket(
-        `wss://${process.env.REACT_APP_API_HOST}:${process.env.REACT_APP_API_PORT}/socket?oid=${data.oid}&token=${encodeURIComponent(data.token)}`,
-      );
-
-      ws.current.addEventListener('message', (ev) => {
-        setMessages((prev) => [...prev, ev.data]);
-      });
-    })();
-
-    return () => {
-      if (ws.current) {
-        ws.current.close();
-      }
-    };
-  }, []);
-
   const sendMessage = () => {
-    if (message && ws.current) {
-      ws.current.send(message);
-      setMessage('');
-    }
+    setMessage('');
   };
 
   return (
-    <div>
-      <input onKeyUp={(e) => e.key === 'Enter' && sendMessage()} value={message} onChange={(e) => setMessage(e.target.value)} />
-      <button onClick={sendMessage} />
-
-      <div>
-        <div>messages:</div>
-        {messages.map((e) => (
-          <div>{e}</div>
+    <Container>
+      <ListContainer>
+        목록
+        {['정품 키즈패딩', '맥북 프로', '장차드', '고급', '리보솜'].map((e, idx) => (
+          <UserWrapper key={e} enable={idx === 2}>
+            <UserImage src="https://via.placeholder.com/25" />
+            <UserName>
+              <span>{e}</span>
+              <span>(홍다우)</span>
+              <div>
+                저 이거 갖고싶어요
+              </div>
+            </UserName>
+          </UserWrapper>
         ))}
-      </div>
-    </div>
+      </ListContainer>
+      <MessageContainer>
+        <input onKeyUp={(e) => e.key === 'Enter' && sendMessage()} value={message} onChange={(e) => setMessage(e.target.value)} />
+
+        <div>
+          <div>messages:</div>
+          {messages.map((e) => (
+            <div>{e}</div>
+          ))}
+        </div>
+      </MessageContainer>
+    </Container>
   );
 };
