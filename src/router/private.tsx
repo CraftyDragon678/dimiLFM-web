9b232285ee1111c8ec54e76d0ba0e946fc1191ff
CraftyDragon678 <craftydragon678@gmail.com>
2020-11-30 23:25:47 +0900

    fix: reduce body container margin

--- src/router/private.tsx
+++ src/router/private.tsx
@@ -8,7 +8,7 @@ import {
 import Header from '../components/Header';
 
 const BodyContainer = styled.div`
-  margin: 0 min(50vw - 300px, 450px);
+  margin: 0 min(50vw - 300px, 250px);
   flex: 1;
   display: flex;
 
