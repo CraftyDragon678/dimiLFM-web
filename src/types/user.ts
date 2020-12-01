5c7dea388385815ac609f6cd9033a06a569ffcf9
CraftyDragon678 <craftydragon678@gmail.com>
2020-12-01 22:26:23 +0900

    feat: getUserDisplayText function

---
+++ src/types/user.ts
@@ -0,0 +1,1 @@
+export type UserType = 'A' | 'D' | 'S' | 'T' | 'G';
---
+++ src/utils/user.ts
@@ -0,0 +1,16 @@
+import { UserType } from 'src/types/user';
+
+interface User {
+  serial?: number;
+  name: string;
+  type: UserType;
+}
+
+export const getUserDisplayText = (user: User) => {
+  if (user.type === 'S') return `${user.serial || ''} ${user.name}`.trim();
+  if (user.type === 'D' || user.type === 'T') return `${user.name}선생님`;
+  if (user.type === 'G') return `졸업생 ${user.name}`;
+  return user.name;
+};
+
+export default {};
