df7f1a19e4ac172b3fc03c5c6db240a9533b8b1f
CraftyDragon678 <craftydragon678@gmail.com>
2020-12-01 20:10:24 +0900

    feat: found article open chat

--- src/pages/article/found.tsx
+++ src/pages/article/found.tsx
@@ -1,25 +1,71 @@
+import styled from '@emotion/styled';
 import { Viewer } from '@toast-ui/react-editor';
 import React, { useEffect, useState } from 'react';
 import { RouteComponentProps } from 'react-router';
 import api from 'src/api';
+import Button from 'src/components/Button';
+import history from 'src/router/history';
+import socket from 'src/socket';
+
+interface FoundData {
+  _id: string;
+  done: boolean;
+  foundLocation: string;
+  from: Date;
+  to: Date;
+  createdAt: Date;
+  updatedAt: Date;
+  title: string;
+  content: string;
+  tag: string;
+  radioIndex: number;
+  user: {
+    serial: number;
+    name: string;
+  };
+}
+
+const Container = styled.div`
+  background-color: white;
+`;
 
 export default ({ match }: RouteComponentProps<{id: string}>) => {
-  const [content, setContent] = useState('');
+  const [article, setArticle] = useState<FoundData>();
   useEffect(() => {
     (async () => {
       const { status, data } = await api.get(`/board/found/${match.params.id}`);
       if (status !== 200) return;
-      setContent(data.content);
+      setArticle(data);
     })();
 
+    const moveToChat = () => {
+      history.push('/chat');
+    };
+    socket.on('open', moveToChat);
+
     return () => {
-      setContent('');
+      setArticle(undefined);
+      socket.off('open', moveToChat);
     };
   }, [match]);
 
+  const contact = () => {
+    if (article) {
+      socket.emit('open', {
+        id: article._id,
+        board: 'found',
+      });
+    }
+  };
+
   return (
-    <>
-      {content && <Viewer initialValue={content} />}
-    </>
+    article ? (
+      <Container>
+        <Viewer initialValue={article.content} />
+        <Button onClick={contact}>연락하기</Button>
+      </Container>
+    ) : (
+      <></>
+    )
   );
 };
