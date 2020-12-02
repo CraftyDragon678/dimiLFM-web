23c34e4127f0690f4c7ceb631f3d650744a3192d
CraftyDragon678 <craftydragon678@gmail.com>
2020-12-02 23:11:41 +0900

    feat: stars component option

--- src/components/Stars.tsx
+++ src/components/Stars.tsx
@@ -4,8 +4,8 @@ import variables from 'src/styles/variables';
 
 interface StarsProp {
   value: number;
-  size: number;
-  onChange: (value: number) => void;
+  size?: number;
+  onChange?: (value: number) => void;
 }
 
 const Wrapper = styled.div`
@@ -19,7 +19,7 @@ const Star = styled.svg<{enabled: boolean}>`
   margin-right: 10px;
 `;
 
-const Stars: React.FC<StarsProp> = ({ value, size, onChange }) => (
+const Stars: React.FC<StarsProp> = ({ value, size = 5, onChange }) => (
   <Wrapper>
     {[...Array(size)].map((_, idx) => (
       <Star
@@ -28,7 +28,7 @@ const Stars: React.FC<StarsProp> = ({ value, size, onChange }) => (
         width="34.7px"
         height="33"
         enabled={idx < value}
-        onClick={() => onChange(idx + 1)}
+        onClick={() => onChange && onChange(idx + 1)}
       >
         <polygon points="17.4,26.8 7.3,32.1 9.2,20.9 1.1,13 12.3,11.3 17.4,1.1 22.4,11.3 33.6,13 25.5,20.9 27.4,32.1" />
       </Star>
