aa008512e585a25194abb683ec03a677c16fe0be
CraftyDragon678 <craftydragon678@gmail.com>
2020-12-02 16:21:19 +0900

    feat: add korean locale (dayjs)

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'codemirror/lib/codemirror.css';
 import '@toast-ui/editor/dist/toastui-editor.css';
+import dayjs from 'dayjs';
 
 import * as serviceWorker from './serviceWorker';
 import App from './App';
 
+import 'dayjs/locale/ko';
 import './socket';
 import './styles/reset.css';
 import './styles/index.css';
 import './styles/Calendar.css';
+
+dayjs.locale('ko');
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
