ea748feeff19f657c2a871820976d549be2f12d0
CraftyDragon678 <craftydragon678@gmail.com>
2020-12-02 22:22:36 +0900

    fix: sellTag -> marketTag, marketTag -> bookTag

--- src/data/tags.ts
+++ src/data/tags.ts
@@ -1,13 +1,13 @@
 export const normalTags = ['옷', '학용품', '전자기기', '택배', '액세서리', '책', '교과서', '문제집', '기타'] as const;
-export const sellTags = ['옷', '학용품', '전자기기', '부품', '액세서리', '기타'] as const;
-export const marketTags = ['책', '문제집', '교과서', '단어장', '공책', '플래너', '기타'] as const;
+export const marketTags = ['옷', '학용품', '전자기기', '부품', '액세서리', '기타'] as const;
+export const bookTags = ['책', '문제집', '교과서', '단어장', '공책', '플래너', '기타'] as const;
 
 export type NormalTagTuple = typeof normalTags;
 export type NormalTag = NormalTagTuple[number];
-export type SellTagTuple = typeof sellTags;
-export type SellTag = SellTagTuple[number];
 export type MarketTagTuple = typeof marketTags;
 export type MarketTag = MarketTagTuple[number];
+export type BookTagTuple = typeof bookTags;
+export type BookTag = BookTagTuple[number];
 
-export type TagTuple = NormalTagTuple | SellTagTuple | MarketTagTuple;
-export type Tag = '태그' | NormalTag | SellTag | MarketTag;
+export type TagTuple = NormalTagTuple | MarketTagTuple | BookTagTuple;
+export type Tag = '태그' | NormalTag | MarketTag | BookTag;
--- src/pages/board/market.tsx
+++ src/pages/board/market.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { normalTags } from 'src/data/tags';
+import { marketTags } from 'src/data/tags';
 import NormalWrapper from './normalWrapper';
 
-export default () => <NormalWrapper type="market" tags={[...normalTags]} />;
+export default () => <NormalWrapper type="market" tags={[...marketTags]} />;
