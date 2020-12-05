f3a0e042754c363b327fb086a4df49058c25abb4
CraftyDragon678 <craftydragon678@gmail.com>
2020-12-05 16:33:55 +0900

    feat: add subjects for board

--- src/data/subjects.ts
+++ src/data/subjects.ts
@@ -1,3 +1,4 @@
 export const subjects = ['국어', '영어', '수학', '사회', '과학', '공업일반', '기초제도', '회계원리', '상업경제', '생활서비스', '인간발달'] as const;
+export const subjectsForBoard = ['전체', '국어', '영어', '수학', '사회', '과학', '기타'] as const;
 export type SubjectTuple = typeof subjects;
 export type Subject = SubjectTuple[number];
